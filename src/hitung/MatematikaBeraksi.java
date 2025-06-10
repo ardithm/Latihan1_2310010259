398717e657ec4ca0cb66d5f179283df39ea58bf0
ardithm <ardianto2300@gmail.com>
2025-06-10 09:25:56 +0800

    Membuat class MatematikaBeraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -1,5 +1,13 @@
 package hitung;
 
 public class MatematikaBeraksi {
-    
+    public static void main(String[] args) {
+        // membuat objek
+        Matematika ardi = new Matematika(5,9);
+        
+        System.out.println("Hasil Penjumlahan: "+ardi.setPenjumlahan());
+        System.out.println("Hasil Pengurangan: "+ardi.setPengurangan());
+        System.out.println("Hasil Perkalian: "+ardi.setPerkalian());
+        System.out.println("Hasil Pembagian: "+ardi.setPembagian());
+    }
 }
